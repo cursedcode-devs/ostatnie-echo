a049962592d07b97710b67b5a48b4543510eebe7
Rekar7 <wojtekbinczyk2003@gmail.com>
2026-02-23 22:39:35 +0100

    Zrobienie porządków z katalogami gry.

---
+++ Assets/Scripts/LogicScripts/ActionManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Bierze input i zamienia go na rodzaj konkretnych akcji w grze
+/// </summary>
+public class ActionManager
+{
+    private Camera mainCamera;
+    private GameObject clickedObject;
+
+    public ActionManager(Camera mainCamera)
+    {
+        this.mainCamera = mainCamera;
+    }
+
+    public GameObject GetClickedObject()
+    {
+        return clickedObject;
+    }
+
+    public ActionTypes GetActionType()
+    {  
+        if (!Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            return ActionTypes.None;
+        }
+        Debug.Log("GetActionType - LeftCLick");
+        clickedObject = ClickedObject(Mouse.current.position.ReadValue());
+
+        return clickedObject != null ? ActionTypes.LeftClickOnObject : ActionTypes.LeftClickOutsiedObject;
+    }
+
+    public GameObject ClickedObject(Vector3 mousePos)
+    {
+        Ray clickingRay = mainCamera.ScreenPointToRay(mousePos);
+
+        if (Physics.Raycast(clickingRay, out RaycastHit raycastHit))
+        {
+            Debug.Log(raycastHit.transform.name);
+            return raycastHit.transform.gameObject;
+        }
+        return null;
+    }
+}
