fc908ac99bbbc46f49e5486fb3c2011d232e18aa
mwirk <m.wirkowski@wp.pl>
2026-04-30 16:50:36 +0200

    UI for shop and partial cassette buying logic

--- Assets/Scripts/DayEndHandler.cs
+++ Assets/Scripts/DayEndHandler.cs
@@ -1,22 +1,25 @@
 using UnityEngine;
-
+using TMPro;
 public class DayEndHandler : MonoBehaviour
 {
     public GameManager gameManager;
     public TimeHandler timeHandler;
+    public Canvas ShopUI;
+    public Canvas ShopButton;
     private GenreValues startListeners;
     private float startMoney;
     public Cassette[] allCassettes;
     public Ad[] allAds;
     public Cassette[] dailyOffer;
     private RadioStation radioStation;
-
     private DaySummaryScreen summaryScreen;
     private GameEndScreen endScreen;
 
     // ------------------------------------------------------------------
     void Start()
-    {
+    {   
+        ShopUI.gameObject.SetActive(false);
+        ShopButton.gameObject.SetActive(false);
         startListeners = new GenreValues
         {
             hipHop = radioStation.currentListeners.hipHop,
@@ -96,9 +99,13 @@ public class DayEndHandler : MonoBehaviour
                 startListeners.metal = radioStation.currentListeners.metal;
                 startMoney = radioStation.GetCurrentMoney();
 
-                GenerateDailyOffer();
+                
+                // GenerateDailyOffer();
 
                 gameManager.SetInputEnabled( true );
+                if (timeHandler.getDay() >= 2 ){
+                    ShopButton.gameObject.SetActive(true);
+                }
             }
         );
     }
@@ -160,8 +167,9 @@ public class DayEndHandler : MonoBehaviour
         dailyOffer = new Cassette[3];
     }
 
-    void GenerateDailyOffer()
+    public void GenerateDailyOffer()
     {
+        gameManager.SetInputEnabled( false );
         if (allCassettes.Length < 3)
         {
             Debug.Log("Not enough cassettes to generate daily offer!");
@@ -182,8 +190,31 @@ public class DayEndHandler : MonoBehaviour
             usedIndexes[usedIndexes.Length - 1 - i] = temp;
         }
 
-        foreach (var c in dailyOffer)
-            Debug.Log("Offered cassette: " + c.name);
+        UpdateMoneySlotInShop();
+
+        for (int i = 1; i <= dailyOffer.Length; i++)
+        {
+            Transform slot = ShopUI.transform.GetChild(i);
+
+            TextMeshProUGUI name = slot.Find("NAZWA")
+                                    .GetComponent<TextMeshProUGUI>();
+
+            TextMeshProUGUI price = slot.Find("CENA")
+                                        .GetComponent<TextMeshProUGUI>();
+
+            TextMeshProUGUI stats = slot.Find("STATYSTYKI")
+                                        .GetComponent<TextMeshProUGUI>();
+
+            name.text = dailyOffer[i-1].name;
+            dailyOffer[i-1].price = Random.Range(10, 100);
+            price.text = dailyOffer[i-1].price.ToString() + " ZŁ";
+            stats.text = dailyOffer[i-1].GetCassetteValues().ToString();
+        }
+        // foreach (var c in dailyOffer)
+        //     Debug.Log("Offered cassette: " + c.name);
+        
+
+        ShopUI.gameObject.SetActive(true);
     }
 
     public void AddCassetteToOffer(Cassette cassette)
@@ -194,5 +225,33 @@ public class DayEndHandler : MonoBehaviour
             newOffer[i + 1] = dailyOffer[i];
         dailyOffer = newOffer;
         Debug.Log($"[DayEndHandler] Dodano '{cassette.name}' do oferty.");
+    }
+    
+
+    public void BuyCassette(int offerIndex){
+        Cassette cassetteToBuy = dailyOffer[offerIndex];
+        float yourMoney= radioStation.GetCurrentMoney();
+        float cassettePrice = cassetteToBuy.price;
+        if (yourMoney >= cassettePrice){
+            radioStation.SetCurrentMoney(yourMoney - cassettePrice);
+            UpdateMoneySlotInShop();
+            Transform slot = ShopUI.transform.GetChild(offerIndex+1);
+            slot.gameObject.SetActive(false);
+        }
+        
+
+
+    }
+
+    public void UpdateMoneySlotInShop(){
+        Transform yourMoneySlot = ShopUI.transform.GetChild(5);
+        TextMeshProUGUI yourMoney = yourMoneySlot.GetComponent<TextMeshProUGUI>();
+        yourMoney.text = radioStation.GetCurrentMoney().ToString();
+    }
+
+    public void ExitShop(){
+        ShopUI.gameObject.SetActive(false);
+        ShopButton.gameObject.SetActive(false);
+        gameManager.SetInputEnabled( false );
     }
 }
--- Assets/Scripts/LogicScripts/RadioStation.cs
+++ Assets/Scripts/LogicScripts/RadioStation.cs
@@ -279,6 +279,10 @@ public class RadioStation
         return currentMoney;
     }
 
+    public void SetCurrentMoney(float newAmountOfMoney){
+        currentMoney = newAmountOfMoney;
+    }
+
     public int GetTotalListeners()
     {
         return currentListeners.totalListeners;
@@ -288,4 +292,5 @@ public class RadioStation
     {
         currentMoney += amount;
     }
+
 }
