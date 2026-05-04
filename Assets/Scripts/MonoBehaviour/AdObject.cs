9585157e00f6f3ba4f89fa0917a4ba8fa4fcd71f
Rekar7 <wojtekbinczyk2003@gmail.com>
2026-05-04 22:59:32 +0200

    Cassette stats are showed when mouse hovers over them

--- Assets/Scripts/MonoBehaviour/AdObject.cs
+++ Assets/Scripts/MonoBehaviour/AdObject.cs
@@ -2,12 +2,5 @@ using UnityEngine;
 
 public class AdObject : PlayableObject
 {
-    void Start()
-    {
-        if (data != null)
-        {
-            gameObject.tag = "Playable";
-            Debug.Log("To jest fizyczna kopia kasety: " + data.name);
-        }
-    }
+   
 }
--- Assets/Scripts/UIScripts/CassetteHoverUI.cs
+++ Assets/Scripts/UIScripts/CassetteHoverUI.cs
@@ -1,11 +1,48 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
 
 public class CassetteHoverUI : MonoBehaviour
 {
+    public GameObject hoverUI;
+    public Camera mainCamera;
+    public TextMeshProUGUI name;
+    public TextMeshProUGUI author;
+    public TextMeshProUGUI stats;
+    public GameObject hoverImage;
+    public RawImage cassetteType;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if(hoverUI != null)
+            hoverUI.SetActive(false);
+    }
+
+    public void MouseHover(PlayableContent cassette, PlayableObject playableObject)
+    {
+        Vector3 screenPosition = mainCamera.WorldToScreenPoint(playableObject.transform.position);
+        screenPosition.x += 140;
+        screenPosition.y -= 90;
+        hoverImage.transform.position = screenPosition;
+        name.text=cassette.GetName();
+        author.text=cassette.GetAuthor();
+        if (cassette.GetType() == CassetteTypes.Ad)
+        {
+            stats.text = "HipHop: " + cassette.GetHipHop() / 100f + "z³" + "\nRock: " + cassette.GetRock() / 100f + "z³" + "\nMetal: " + cassette.GetMetal() / 100f + "z³" + "\nDisco: " + cassette.GetDisco() / 100f + "z³";
+        }
+        else if (cassette.GetType() == CassetteTypes.Music)
+        {
+            stats.text = "HipHop: " + cassette.GetHipHop() / 100f + "\nRock: " + cassette.GetRock() / 100f + "\nMetal: " + cassette.GetMetal() / 100f + "\nDisco: " + cassette.GetDisco() / 100f;
+        }
+        hoverUI.SetActive(true);
+    }
+
+    public void MouseLeft()
+    {
+        hoverUI.SetActive(false);
     }
 
     // Update is called once per frame
