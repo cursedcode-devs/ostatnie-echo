480284a8af4633d8595ddf3d5f677c8926726534
tobgrz <grz.to@protonmail.com>
2026-06-03 21:22:47 +0200

    Add listeners charts

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -1,13 +1,29 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class GenreBarUI
+{
+    public TextMeshProUGUI amountText;
+    public RectTransform barFill;
+}
 
 public class StatsUI : MonoBehaviour
 {
-    public TextMeshProUGUI moneyText;
-    public TextMeshProUGUI hipHopText;
-    public TextMeshProUGUI rockText;
-    public TextMeshProUGUI popText;
-    public TextMeshProUGUI discoText;
+    public TextMeshProUGUI budgetText;
+    
+    [Header("Bars Configuration")]
+    public float maxBarHeight = 250f;
+    [Tooltip("If true, max listeners scales automatically to the highest current listener count.")]
+    public bool dynamicMaxListeners = true;
+    public int manualMaxListeners = 500;
+
+    [Header("Genre Bars")]
+    public GenreBarUI popBar;
+    public GenreBarUI rockBar;
+    public GenreBarUI hipHopBar;
+    public GenreBarUI discoBar;
 
     private RadioStation radioStation;
 
@@ -24,10 +40,35 @@ public class StatsUI : MonoBehaviour
 
     public void UpdateUI()
     {
-        moneyText.text  = "Money: "  + radioStation.GetCurrentMoney().ToString("F2") + "$";
-        hipHopText.text = "HipHop: " + radioStation.currentListeners.hipHop;
-        rockText.text   = "Rock: "   + radioStation.currentListeners.rock;
-        popText.text  = "Pop: "  + radioStation.currentListeners.pop;
-        discoText.text  = "Disco: "  + radioStation.currentListeners.disco;
+        budgetText.text = $"BUDŻET: {radioStation.GetCurrentMoney():F0} zł";
+
+        int pop = radioStation.currentListeners.pop;
+        int rock = radioStation.currentListeners.rock;
+        int hipHop = radioStation.currentListeners.hipHop;
+        int disco = radioStation.currentListeners.disco;
+
+        int currentMax = manualMaxListeners;
+        if (dynamicMaxListeners)
+        {
+            currentMax = Mathf.Max(pop, rock, hipHop, disco);
+            if (currentMax < 10) currentMax = 10; // Prevent division by zero
+        }
+
+        UpdateBar(popBar, pop, currentMax);
+        UpdateBar(rockBar, rock, currentMax);
+        UpdateBar(hipHopBar, hipHop, currentMax);
+        UpdateBar(discoBar, disco, currentMax);
+    }
+
+    private void UpdateBar(GenreBarUI bar, int amount, int currentMax)
+    {
+        if (bar.amountText != null)
+            bar.amountText.text = amount.ToString();
+            
+        if (bar.barFill != null)
+        {
+            float fillRatio = Mathf.Clamp01((float)amount / currentMax);
+            bar.barFill.sizeDelta = new Vector2(bar.barFill.sizeDelta.x, fillRatio * maxBarHeight);
+        }
     }
 }
